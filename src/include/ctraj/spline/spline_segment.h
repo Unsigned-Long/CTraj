099f6b99016656068df3bc663849c36a254b718e
unsigned-long <3079625093@qq.com>
2023-07-07 13:24:51 +0800

    update copyright

--- src/include/ctraj/spline/spline_segment.h
+++ src/include/ctraj/spline/spline_segment.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2023. Created on 7/7/23 1:20 PM by shlchen@whu.edu.cn (Shuolong Chen), who received the B.S. degree in
+// geodesy and geomatics engineering from Wuhan University, Wuhan China, in 2023. He is currently a master candidate at
+// the school of Geodesy and Geomatics, Wuhan University. His area of research currently focuses on integrated navigation
+// systems and multi-sensor fusion.
+
 #pragma once
 
 #include <Eigen/Dense>
--- src/src/view/traj_viewer.cpp
+++ src/src/view/traj_viewer.cpp
@@ -1,6 +1,7 @@
-//
-// Created by csl on 10/7/22.
-//
+// Copyright (c) 2023. Created on 7/7/23 1:19 PM by shlchen@whu.edu.cn (Shuolong Chen), who received the B.S. degree in
+// geodesy and geomatics engineering from Wuhan University, Wuhan China, in 2023. He is currently a master candidate at
+// the school of Geodesy and Geomatics, Wuhan University. His area of research currently focuses on integrated navigation
+// systems and multi-sensor fusion.
 
 #include "ctraj/view/traj_viewer.h"
 
